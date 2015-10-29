87906eae215eaa9c1b75a2dd19cb4a33a84db0aa
Matt Mazzola <mattmazzola@live.com>
2015-10-29 00:27:02 -0700

    Add sessionAccount reference to application to show currently signed in user and update sessionAccount to extract userId until we have user models.

--- app/services/session-account.js
+++ app/services/session-account.js
@@ -1,15 +1,21 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Service.extend({
   session: Ember.inject.service('session'),
   store: Ember.inject.service('store'),
 
-  account: Ember.computed('session.data.authenticated.account_id', function() {
-    const accountId = this.get('session.data.authenticated.account_id');
-    if (!Ember.isEmpty(accountId)) {
-      return DS.PromiseObject.create({
-        promise: this.get('store').find('account', accountId)
-      });
-    }
+  userId: Ember.computed('session.data.authenticated.userId', function() {
+    const userId = this.get('session.data.authenticated.userId');
+    return userId;
   })
+
+  // userId: Ember.computed('session.data.authenticated.userId', function() {
+  //   const accountId = this.get('session.data.authenticated.userId');
+  //   if (!Ember.isEmpty(accountId)) {
+  //     return DS.PromiseObject.create({
+  //       promise: this.get('store').find('account', accountId)
+  //     });
+  //   }
+  // })
 });
